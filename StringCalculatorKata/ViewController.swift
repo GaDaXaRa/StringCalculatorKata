82e6a652913016b58239c1f9e593d92448956238
gadaxara <gadaxara@gmail.com>
2016-06-27 17:53:26 +0200

    Initial Commit

---
+++ StringCalculatorKata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StringCalculatorKata
+//
+//  Created by Miguel Santiago Rodríguez on 27/6/16.
+//  Copyright © 2016 Miguel Santiago Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
